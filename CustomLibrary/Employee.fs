cb7559f32e62bcc4baa9bd8d609022bdc81aaaf0
Tim Löfström <tim.lofstrom.danielsson@lm.se>
2021-12-10 15:59:02 +0100

    update

---
+++ CustomLibrary/Employee.fs
@@ -0,0 +1,7 @@
+namespace CustomLibrary
+
+open System
+
+type Employee =
+
+    val age : int
--- CustomLibrary/Library.fs
+++ CustomLibrary/Library.fs
@@ -1,5 +1,40 @@
 ﻿namespace CustomLibrary
+open Newtonsoft.Json
 
 module Say =
+
+    let test = 
+        printfn "Test"
+
     let hello name =
         printfn "Hello %s" name
+
+    let convertToJson str =
+        JsonConvert.SerializeObject str
+
+    let add x y = x + y
+
+    let square x = x * x
+
+    let mutExample =
+        let mutable x = 5
+        x <- 6
+        x
+
+    let listExample =
+        let listTest = [1;2;3]
+        listTest
+
+    let inc = add 1
+
+    let mapExample =
+        listExample |> List.map inc |> List.map square
+
+
+    let incSquare = (List.map inc) >> (List.map square)
+
+    let inSquareTester =
+        incSquare listExample
+
+    let compare a b =
+        a > b
